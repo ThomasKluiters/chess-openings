e21a204f3468f24d4d12d1e856b313c34422c442
thomas <thomas.kluiters@gmail.com>
2019-05-12 15:08:29 +0200

    Create type properties for ChessBoard

--- src/Chessboard.tsx
+++ src/Chessboard.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Chessground from 'chessground'
 import { Route } from 'react-router-dom';
+import { Chess } from 'chess.js';
 
-class Chessboard extends React.Component {
+export interface ChessboardProps extends React.HTMLProps<HTMLDivElement> {
+  [key: string]: any;
+  tag?: React.ReactType;
+  
+  fen?: string
+}
+
+class Chessboard extends React.Component<ChessboardProps> {
+  
+  constructor(props: ChessboardProps) {
+    super(props);
+  }
+
   render() {
     return <p></p>
   }
--- src/Explore.tsx
+++ src/Explore.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route } from 'react-router-dom';
+import Chessboard from './Chessboard';
 
 
 const Explore: React.FC = () => {
     return (
       <Route path="/explore" render={(props) => (
-        <h1>Hello world!</h1>
+        <Chessboard></Chessboard>
       )} />
     );
   }
