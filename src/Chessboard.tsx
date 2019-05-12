76e97f4d85e6499b72b988bb7fb25cc18aa312ae
thomas <thomas.kluiters@gmail.com>
2019-05-12 14:35:18 +0200

    Install chess dependencies and create Chessboard component

---
+++ src/Chessboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route } from 'react-router-dom';
+
+class Chessboard extends React.Component {
+  render() {
+    return <p></p>
+  }
+}
+
+export default Chessboard;
