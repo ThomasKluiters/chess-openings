a0dc69499c76f57bab017e36f8b0dab14480b5a0
Thomas Kluiters <thomas.kluiters@gmail.com>
2019-05-15 19:53:53 +0200

    Add images and theme

--- src/Chessboard.tsx
+++ src/Chessboard.tsx
@@ -1,10 +1,10 @@
 import React, { RefObject } from 'react';
 import { Chessground } from 'chessground'
 import { Api } from 'chessground/api'
-import { ChessInstance } from 'chess.js';
-import ReactDOM from 'react-dom';
+import { ChessInstance, Square } from 'chess.js';
 
 import './Chessground.css'
+import { Key } from 'chessground/types';
 
 const Chess = require('chess.js')
 
@@ -27,17 +27,59 @@ class Chessboard extends React.Component<ChessboardProps> {
     super(props);
 
     this.container = React.createRef<HTMLDivElement>()
+
+    this.computeMoves = this.computeMoves.bind(this);
+    this.flipColor = this.flipColor.bind(this);
+    this.toDests = this.toDests.bind(this);
   }
   
+  toDests() {
+    var dests: { [from: string]: Square[] ; } = {};
+    this.chess!.SQUARES.forEach(s => {
+      const ms = this.chess!.moves({square: s, verbose: true});
+      if (ms.length) dests[s] = ms.map(m => m.to);
+    });
+    return dests;
+  }
+
+  flipColor() {
+    return (this.chess!.turn() === 'w') ? 'white' : 'black';
+  }
+
+  computeMoves(orig: Key, dest: Key) {
+      this.chess!.move({from: orig as Square, to: dest as Square});
+      this.api!.set({
+        turnColor: this.flipColor(),
+        movable: {
+          color: this.flipColor(),
+          dests: this.toDests()
+        }
+      });
+  }
+
   render() {
-    return <div ref={this.container}/>;
+    return (
+      <div className="merida">
+        <div ref={this.container}/>
+      </div>
+    );
   }
 
   componentDidMount() {
     const node = this.container.current as HTMLElement;
 
-    this.chess = new Chess(this.props.fen);
-    this.api = Chessground(node) ;
+    this.chess = new Chess();
+    this.api = Chessground(node, {
+      movable: {
+        color: 'white',
+        free: false,
+        dests: this.toDests(),
+        events: { after: this.computeMoves }
+      },
+      draggable: {
+        showGhost: true
+      }
+    });
   }
 }
 
